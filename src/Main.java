ff268369b06c36a13aa4423b58357f1bec9b7b68
repo22 <cadinica@yahoo.com>
2022-12-10 11:41:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        String CNP = "5230601050023";
+
+        if (CNP.length() != 13) {
+            System.out.println("CNP is not valid");
+        }
+    }
+}
+
+
+
